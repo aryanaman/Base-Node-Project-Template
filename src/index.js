20e8280560d7c693dd1ae19cb0be7acfdb80da88
aryanaman <amanaryankr@gmail.com>
2023-06-03 12:10:27 +0530

    added folder template for any big project

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const { serverConfig, Logger } = require("./config");
 const apiRoutes = require("./routes");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", apiRoutes);
 app.listen(serverConfig.PORT, () => {
   console.log("successfully run on the port", serverConfig.PORT);
